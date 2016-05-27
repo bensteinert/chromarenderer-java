6e2bddce25286c80a8e469dcc88fb7acd4db8814
bensteinert <benjamin.steinert@googlemail.com>
2016-05-27 16:33:09 +0200

    Adds new test scene

---
+++ src/main/java/net/chromarenderer/renderer/scene/FurnaceTest.java
@@ -0,0 +1,44 @@
+package net.chromarenderer.renderer.scene;
+
+import net.chromarenderer.math.COLORS;
+import net.chromarenderer.math.ImmutableVector3;
+import net.chromarenderer.math.Vector3;
+import net.chromarenderer.math.geometry.Sphere;
+import net.chromarenderer.math.raytracing.Hitpoint;
+import net.chromarenderer.math.raytracing.Ray;
+import net.chromarenderer.renderer.shader.Material;
+
+/**
+ * @author bensteinert
+ */
+public class FurnaceTest implements ChromaScene {
+
+    Sphere innerSphere = new Sphere(Vector3.ORIGIN, 1.0f, Material.createDiffuseMaterial(COLORS.GREY));
+    Sphere outerSphere = new Sphere(Vector3.ORIGIN, 100000000.0f, Material.createDiffuseMaterial(COLORS.WHITE));
+
+    @Override
+    public Hitpoint intersect(Ray ray) {
+        float distance = innerSphere.intersect(ray);
+        if (ray.isOnRay(distance)) {
+            final ImmutableVector3 point = ray.onRay(distance);
+            return new Hitpoint(innerSphere, point, innerSphere.getNormal(point));
+        } else {
+            float envHit = outerSphere.intersect(ray);
+            final ImmutableVector3 point = ray.onRay(envHit);
+            return new Hitpoint(outerSphere, point, outerSphere.getNormal(point).mult(-1.0f));
+        }
+    }
+
+
+    @Override
+    public Hitpoint getLightSourceSample() {
+        final ImmutableVector3 sample = outerSphere.getUnifDistrSample();
+        return new Hitpoint(outerSphere, sample, outerSphere.getNormal(sample).mult(-1.0f));
+    }
+
+
+    @Override
+    public boolean isObstructed(Ray shadowRay) {
+        return false;
+    }
+}
